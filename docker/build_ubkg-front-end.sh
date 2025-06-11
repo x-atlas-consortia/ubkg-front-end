cc3706d9097385800b19b50ca9cbade598f277b4
AlanSimmons <jas971@pitt.edu>
2025-06-11 00:10:01 -0400

    build with issue: original request parameters not being passed to upstream in auth_request

--- docker/build_ubkg-front-end.sh
+++ docker/build_ubkg-front-end.sh
@@ -7,5 +7,5 @@ if [ "$1" = "build" ]; then
   docker compose -f development-docker-compose.yml -f docker-compose.yml -p ubkg-front-end build
 elif [ "$1" = "push" ]; then
   # buildx uses docker-compose.yml to publish in multiple architectures
-  docker buildx bake -f docker-compose.yml  --push
+  docker buildx bake -f development-docker-compose.yml -f docker-compose.yml  --push
 fi
