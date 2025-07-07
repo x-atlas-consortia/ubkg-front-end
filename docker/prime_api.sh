2e8c69d54c19486d60bd37a7ee73412e8fe6d3b6
AlanSimmons <jas971@pitt.edu>
2025-07-07 11:37:19 -0400

    ubkg-front-end now forces the neo4j instance in ubkg-back-end to populate its query plan cache via calls to the ubkg-api.

---
+++ docker/prime_api.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -e
+set -u
+
+#########
+# neo4j INITIALIZATION SCRIPT
+# When a Cypher query is executed for the first time in a new neo4j instance, neo4j
+# must add the query plan to its plan cache. Plan caching results in the initial
+# execution of the query taking longer than subsequent executions.
+# In the ubkg-api in UBKGBox, the initial execution fails with a HTTP 500 error.
+# A workaround is to execute a set of known queries immediately after the neo4j
+# instance is up, similar to priming a pump. In particular, this mitigates the risk
+# of the Guesdt application failing in the initial load of its index.html page, because
+# that page populates with content from the ubkg-api.
+##########
+
+# In lieu of depending on the healthcheck in Docker
+sleep 30
+
+echo "UBKG-api initialization script"
+
+UBKG_URL=http://ubkg-api:8080
+
+# Guesdt specific
+echo "calling: /codes/FMA:7149/codes"
+curl --request GET \
+ --url "${UBKG_URL}/codes/FMA:7149/codes" \
+ --header "Accept: application/json" | cut -c1-60
+
+echo "calling: /codes/FMA:7149/concepts"
+curl --request GET \
+ --url "${UBKG_URL}/codes/FMA:7149/concepts" \
+ --header "Accept: application/json" | cut -c1-60
+
+echo "calling: /codes/FMA:7149/terms"
+curl --request GET \
+ --url "${UBKG_URL}/codes/FMA:7149/terms" \
+ --header "Accept: application/json" | cut -c1-60
+
+echo "calling: /concepts/C0460002/concepts"
+curl --request GET \
+ --url "${UBKG_URL}/concepts/C0460002/concepts" \
+ --header "Accept: application/json" | cut -c1-60
+
+# Other
+echo "calling: /concepts/C0460002/codes"
+curl --request GET \
+ --url "${UBKG_URL}/concepts/C0460002/codes" \
+ --header "Accept: application/json" | cut -c1-60
+
+echo "calling: /concepts/C0460002/definitions"
+curl --request GET \
+ --url "${UBKG_URL}/concepts/C0460002/definitions" \
+ --header "Accept: application/json" | cut -c1-60
+echo
+echo
+
+echo "calling: /concepts/C0460002/nodeobjects"
+curl --request GET \
+ --url "${UBKG_URL}/concepts/C0460002/nodeobjects" \
+ --header "Accept: application/json" | cut -c1-60
+
+echo "Initialization complete"
+echo "UBKGBox is ready"
+
--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,10 @@
 # 'daemon off;' is nginx configuration directive
 nginx -g 'daemon off;' &
 
+# Initialize the query plan cache of the neo4j instance in UBKGBox by executing
+# queries via the ubk-api.
+./prime_api.sh
+
 # Command to keep the container running indefinitely so as not to exit with code 0.
 # This is a divergence from the hubmapconsortium standard api docker build. Docker containers
 # of Flask APIs run uwsgi, which keeps the container open.
